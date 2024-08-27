77ab431ee9ceb9a40cffbdd6b75f7d1907b2ef4f
Алёна <elapr943@gmail.com>
2024-08-27 22:26:39 +0300

    add-states

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import { Container, Row, Col, Form, Button, InputGroup } from 'react-bootstrap';
 import { CopyToClipboard } from 'react-copy-to-clipboard'; 
 
 function App() {
-  
+  const [password, setPassword] = useState('');
+  const [length, setLength] = useState(12);
+  const [copied, setCopied] = useState(false);
+  const [options, setOptions] = useState({
+    uppercase: true,
+    lowercase: true,
+    numbers: true,
+    symbols: true,
+  });
 }
 
 export default App;
